3ff934ff1a1fb4d11bbdfb72c8f23e1797688a2e
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2024-11-21 23:40:01 +0100

    60-65 subscriptions POST no db

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,30 @@ use std::net::TcpListener;
 
 use actix_web::{
     dev::Server,
-    web::{self},
+    web::{self, Form},
     App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FromData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(form: web::Form<FromData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    Ok(
-        HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-            .listen(listener)?
-            .run(),
-    )
+    Ok(HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run())
 }
